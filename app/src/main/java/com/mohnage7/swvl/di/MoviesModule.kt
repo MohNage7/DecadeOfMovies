32f1db8b810d4442e589fbca2898a9a4b15d9ae0
Mohamed Nageh <>
2020-07-02 22:25:51 +0200

    feat - Added data and usecase layers depencencies to app module - Added movies.json file - Added Koin depencency - Added di layer and finish all the required injection for the components

---
+++ app/src/main/java/com/mohnage7/swvl/di/MoviesModule.kt
@@ -0,0 +1,16 @@
+package com.mohnage7.swvl.di
+
+import com.mohnage7.data.MoviesRepository
+import com.mohnage7.swvl.framework.data.MoviesLocalDataSourceImpl
+import com.mohnage7.swvl.presentation.movies.viewmodel.MoviesViewModel
+import com.mohnage7.usecase.GetMoviesUseCase
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+val moviesModule = module {
+    single { MoviesLocalDataSourceImpl(get()) }
+    single { MoviesRepository(get<MoviesLocalDataSourceImpl>()) }
+    single { GetMoviesUseCase(get()) }
+    viewModel { MoviesViewModel(get()) }
+}
