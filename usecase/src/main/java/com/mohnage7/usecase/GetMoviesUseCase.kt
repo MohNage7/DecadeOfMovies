39b69754ac5f50943e1832f9f5c3dc84505b207e
Mohamed Nageh <>
2020-07-02 22:24:07 +0200

    feat - Added usecase layer that will leverage data layer to get the data and deliver it to the ViewModel - Finished GetMoviesUseCase that fetches local movies from the file and sort it by year

---
+++ usecase/src/main/java/com/mohnage7/usecase/GetMoviesUseCase.kt
@@ -0,0 +1,8 @@
+package com.mohnage7.usecase
+
+import com.mohnage7.data.MoviesRepository
+import com.mohnage7.domain.LocalMovie
+
+class GetMoviesUseCase(private val repository: MoviesRepository) {
+    operator fun invoke(): List<LocalMovie> = repository.getMovies().sortedBy { it.year }
+}
